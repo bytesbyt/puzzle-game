7043234835e9f40340d98caf9d59b6f21d91d4ac
Tes <101018180+bytesbyt@users.noreply.github.com>
2024-01-10 20:26:41 +0000

    Add function to shuffle tiles after 2 seconds

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,38 @@ const playTime = document.querySelector(".play-time")
 //Number of tiles
 const tileCount = 16;
 
-Array(tileCount).fill().forEach( (_, i) => {
-    const li = document.createElement("li");
-    li.setAttribute('data-index', i)
-    li.classList.add(`list${i}`);
-    container.appendChild(li)
-})
+let tiles = [];
 
-// Shuffle puzzle pieces
+//Sety to show puzzle image first and shuffle after 2secs
+setGame();
+
+function setGame(){
+    tiles = createImageTiles();
+    tiles.forEach(tile => container.appendChild(tile))
+    setTimeout(() => {
+        container.innerHTML = "";
+        shuffle(tiles).forEach(tile => container.appendChild(tile))
+    },2000)
+}
+
+function createImageTiles(){
+    const tempArray = [];
+    Array(tileCount).fill().forEach( (_, i) => {
+        const li = document.createElement("li");
+        li.setAttribute('data-index', i)
+        li.classList.add(`list${i}`);
+        tempArray.push(li)
+    })
+    return tempArray;
+}
+
+// Shuffle puzzle tiles
 function shuffle(array){
     let index = array.length -1;
     while(index > 0){
         const randomIndex = Math.floor(Math.random()*(index+1));
+        [array[index], array[randomIndex]] = [array[randomIndex], array[index]]
         index--;
     }
+    return array;
 }
